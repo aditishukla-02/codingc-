bf114a2b21d7d77173c54819f0d4aeeaa6fe288c
aditishukla-02 <shukladiti037@gmail.com>
2024-11-12 15:48:06 +0530

    Add files via upload

---
+++ linearsearch.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int key =2;
+    int i;
+
+    int arr[20]={2,-1,20,44,55,66,677};
+    for(int i=0;i<20;i++){
+        if(key==arr[i]){
+           return i ;
+        }
+        else {
+            return 0;
+        }
+
+    }
+  cout<<i;
+}
